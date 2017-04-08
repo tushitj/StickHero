85e8b2647cc9ef212d25b7e1643d8418b5b3f306
Tushit Jain <tushitjain@Tushits-MacBook-Air.local>
2017-04-07 20:35:44 -0400

    settings

--- project6/project6/GameScene.swift
+++ project6/project6/GameScene.swift
@@ -10,9 +10,9 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
     override func didMove(to view: SKView) {
-    }
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
