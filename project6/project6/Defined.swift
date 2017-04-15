0e1a7ac6db5a62238bb0ea8418ce0e1b856230bb
Tushit Jain <tjain3@binghamton.edu>
2017-04-14 21:30:30 -0400

    new File added

---
+++ project6/project6/Defined.swift
@@ -0,0 +1,13 @@
+//
+//  Defined.swift
+//  project6
+//
+//  Created by Tushit Jain on 4/14/17.
+//  Copyright © 2017 Tushit Jain. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+let DefinedScreenWidth:CGFloat = 1536
+let DefinedScreenHeight:CGFloat = 2048
